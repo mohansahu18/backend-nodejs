828b1fc506caf011c65f112c9acce9359d92f577
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-19 00:27:11 +0530

    local file routes added

--- file upload/index.js
+++ file upload/index.js
@@ -1,12 +1,21 @@
 const express = require("express")
 require('dotenv').config()
 const dbConnection = require('./config/database')
+const fileUpload = require("express-fileupload")
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.use(express.json())
+
+app.use(fileUpload())
+
+const router = require("./routes/fileupload")
+app.use('/api/v1', router)
+
+const cloudinary = require("./config/cloudinary")
+cloudinary()
 
 app.listen(PORT, () => {
     console.log(`server started on port no ${PORT}`);
